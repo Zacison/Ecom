5c72f6adb55e6e338fd7d1fe0d9abb9a2e1e34fe
Zach Dantzer <zdantzer@yahoo.com>
2020-11-01 16:42:03 -0600

    first commit, have home and shop pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/Homepage';
+import ShopPage from './pages/shop/Shop';
+import Header from './components/header/header';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/shop/" component={ShopPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
