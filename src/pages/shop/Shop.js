213375d480d16f63e65a60ce28c080ad5bf3ddca
Zach Dantzer <zdantzer@yahoo.com>
2020-12-12 21:45:36 -0600

    added data persistence for the cart items

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+import { selectCollections } from '../../redux/shop/shop.selector';
 import CollectionPreview from '../../components/collection-preview/collection-preview';
+//import { connect } from 'react-redux';
+//import { createStructuredSelector } from 'reselect';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
 
-class ShopPage extends React.Component {
-    state = {
-        collections: SHOP_DATA,
-    };
-
-    render() {
-        let collections = this.state.collections;
-        return (
-            <div className="shop-page">
-                {collections.map(({ id, ...otherCollectionProps }) => {
-                    return (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    );
-                })}
-            </div>
-        );
-    }
-}
+const ShopPage = ({ collections }) => {
+    return (
+        <div className="shop-page">
+            <CollectionsOverview />
+        </div>
+    );
+};
 
 export default ShopPage;
