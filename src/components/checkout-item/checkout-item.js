1e82c17a48b02c8e5f22831c58c1d2e5f1ba94c4
Zach Dantzer <zdantzer@yahoo.com>
2020-12-14 17:43:07 -0600

    fixed small bug in cart

--- src/components/checkout-item/checkout-item.js
+++ src/components/checkout-item/checkout-item.js
@@ -9,7 +9,6 @@ import {
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
-
     return (
         <div className="checkout-item">
             <div className="image-container">
@@ -17,28 +16,32 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
             </div>
             <span className="name">{name}</span>
             <span className="quantity">
-                <div className="arrow" onClick={() => addItem()}>
+                <div
+                    className="arrow"
+                    onClick={() => {
+                        console.log('remove Item');
+                        removeItem(cartItem);
+                    }}
+                >
                     &#10094;
                 </div>
                 <span className="value">{quantity}</span>
-                <div className="arrow" onClick={() => removeItem()}>
+                <div className="arrow" onClick={() => addItem(cartItem)}>
                     &#10095;
-                </div>{' '}
+                </div>
             </span>
-            <span className="price"> {price} </span>
-            <span className="remove-button" onClick={() => clearItem(cartItem)}>
+            <span className="price">{price}</span>
+            <div className="remove-button" onClick={() => clearItem(cartItem)}>
                 &#10005;
-            </span>
+            </div>
         </div>
     );
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        clearItem: (item) => dispatch(clearItemFromCart(item)),
-        addItem: (item) => dispatch(addItem(item)),
-        removeItem: (item) => dispatch(removeItem(item)),
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+    clearItem: (item) => dispatch(clearItemFromCart(item)),
+    addItem: (item) => dispatch(addItem(item)),
+    removeItem: (item) => dispatch(removeItem(item)),
+});
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
